1e6d1eb95599d87f5b5e419f05d153b03cd7d3b4
FredoSimanjuntak <mahezzeham@gmail.com>
2024-09-28 21:47:50 +0700

    Update apalah.java
    menambahkan paragraf 2

--- apalah.java
+++ apalah.java
@@ -1,5 +1,6 @@
 public class apalah{
   public static void main {String []args} {
-    System.out.print("hallo world!")
+    System.out.print("hallo world!");
+    System.out.print(""hallu yalal yalili)
   }
 }
